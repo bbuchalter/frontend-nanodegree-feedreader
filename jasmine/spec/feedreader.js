0f59b95393808d1fa4a8fbbd3962f915e38dd6ee
Brian Buchalter <bal711@gmail.com>
2015-04-18 07:35:34 -0600

    Implement allFeeds tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,19 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+        it('has a URL', function() {
+            allFeeds.forEach(function(feed) {
+              expect(feed.url).toBeDefined();
+              expect(feed.url).not.toBe("");
+            });
+        });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        it('has a name', function() {
+          allFeeds.forEach(function(feed) {
+            expect(feed.name).toBeDefined();
+            expect(feed.name).not.toBe("");
+          });
+        });
     });
 
 
